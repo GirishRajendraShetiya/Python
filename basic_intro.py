54231725b313a2aa4d52a9df11935dfa3a138377
GirishRajendraShetiya <girish4576142@gmail.com>
2026-05-28 08:26:24 +0530

    Add basic_intro file

---
+++ basic_intro.py
@@ -0,0 +1,8 @@
+# Python is an OBJECT-ORIENTED language.
+# Object has the following types: Identity, Type & Value
+
+# Through identity, mutability (changeable) and immutability (non-changeable) could be figured out.
+
+# Immutability
+
+
