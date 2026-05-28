98bfcc44b5973db8fbf23bb3fad4bb0186ee49dc
GirishRajendraShetiya <girish4576142@gmail.com>
2026-05-28 09:29:23 +0530

    Update the basic_intro file

--- basic_intro.py
+++ basic_intro.py
@@ -1,8 +1,32 @@
-# Python is an OBJECT-ORIENTED language.
+# Python is an OBJECT-ORIENTED language - Everything in Python is in the form of an "object" which means a data structure, a way of storing data.
+# REPL (Read, Evaluate, Print, Loop)
+
 # Object has the following types: Identity, Type & Value
 
 # Through identity, mutability (changeable) and immutability (non-changeable) could be figured out.
 
 # Immutability
 
+num_1 = 10
+print(num_1)  # 10
+print(id(num_1))  # 140265468717416
 
+num_1 = 20
+print(num_1)  # 20
+print(id(num_1))  # 140265468717736
+
+# The same variable num_1 points to 2 different references (10 & 20), as integers can’t be modified (immutable).
+# In Python, variables aren't boxes that hold data; they are labels (or pointers) that stick to objects floating around in computer memory.
+
+# It rips the label num_1 off of the 10 and sticks it onto the 20.
+# The integer 10 is still sitting in memory (at least until Python's garbage collector cleans it up), but the variable num_1 has completely lost track of it.
+
+# Mutability
+
+set_1 = set()
+print(set_1)  # set() - Empty set is created
+
+set_1.add(10)
+print(set_1)  # {10}
+
+# The set_1 variable is pointing to same reference even after updating it, as it’s modifiable (mutable).
