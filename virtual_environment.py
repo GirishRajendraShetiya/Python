51e1d0798b773dd3394f69ba0ab2953d3f557315
GirishRajendraShetiya <54206883+GirishRajendraShetiya@users.noreply.github.com>
2026-06-01 05:21:15 +0000

    Add virtual_environment

---
+++ virtual_environment.py
@@ -0,0 +1,23 @@
+# PyPI (Python Package Index) - It's a repository of software for Python programming.
+# Our created projects could be uploaded here (pypi.org).
+
+# pip(Pip Installs Packages) - used to install packages in Python.
+
+# Commands:
+
+# pip install <package_name>: To install the latest package.
+# pip –version: To get the version installed on the system.
+# pip list: To get the list of packages installed
+# pip uninstall <package_name>: To uninstall a package.
+# pip install <package_name>==version: To install a specific version as per the requirement.
+# pip freeze > requirements.txt: To create the requirements.txt file
+# pip freeze | xargs pip uninstall -y: To uninstall all the installed packages.
+# pip install -r requirements.txt: To install a bunch of packages (provide the path of the .txt file if not in the same directory)
+
+# To create various environments in Python for different projects as per the requirement.
+
+# python -m venv .venv
+# .\.venv\Scripts\activate
+
+# The virtual environment's name can be seen on the left side of the terminal.
+# To de-activate the environment, type deactivate.
